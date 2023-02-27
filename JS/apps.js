7e645b1411441a9637902fd7513801c2c9713c27
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-27 21:20:46 +0600

    get data in button

--- JS/apps.js
+++ JS/apps.js
@@ -1,6 +1,6 @@
 // console.log('connect')
 const loadMealDb = (searchInput) => {
-    const URL =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+    const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
     fetch(URL)
         .then(res => res.json())
         .then(data => showDAta(data.meals))
@@ -8,8 +8,8 @@ const loadMealDb = (searchInput) => {
 const showDAta = (data) => {
     const cardContainer = document.getElementById('food-card-group')
     cardContainer.innerHTML = '';
-    console.log(data)
-    data.forEach( food => {
+    // console.log(data)
+    data.forEach(food => {
         console.log(food)
         const creatDiv = document.createElement('div')
         creatDiv.innerHTML = `
@@ -20,6 +20,10 @@ const showDAta = (data) => {
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in
                     to additional content. This content is a little bit longer.</p>
             </div>
+            <!-- Button trigger modal -->
+            <button onclick="loadModalDetail('${food.idMeal}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#shodDetailsModal">
+            Details
+            </button>
        </div>
         `;
         cardContainer.appendChild(creatDiv)
@@ -31,6 +35,17 @@ document.getElementById('search-btn').addEventListener('click', function () {
     console.log(searchInput)
     loadMealDb(searchInput)
 })
+
+const loadModalDetail = (id) => {
+    const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => console.log(data.meals[0]))
+}
+
+const showDetailsModal = () => {
+
+}
 
 
 loadMealDb('fish')
